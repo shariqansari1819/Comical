da902084dc78eecf3718356c73f70e7e0c2831d0
Shariq Ansari <mshariq@vodworks.com>
2019-08-28 16:10:18 +0500

    Added clicks listener in login activity.

--- app/src/main/java/com/codebosses/comical/activities/LoginActivity.java
+++ app/src/main/java/com/codebosses/comical/activities/LoginActivity.java
@@ -1,16 +1,42 @@
 package com.codebosses.comical.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.codebosses.comical.R;
+import com.codebosses.comical.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //    Android fields....
+    private ActivityLoginBinding loginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        loginBinding.setClickHandler(new LoginClickHandler());
+
+    }
+
+    public class LoginClickHandler {
+
+        public void onLoginClick(View view) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+
+        public void onFacebookClick(View view) {
+
+        }
+
+        public void onGoogleClick(View view) {
+
+        }
+
     }
 }
