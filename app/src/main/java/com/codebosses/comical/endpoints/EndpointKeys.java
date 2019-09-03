b6dbc23964198d00379cd7b729a3471583f22c5d
Shariq Ansari <mshariq@vodworks.com>
2019-09-03 16:33:46 +0500

    Added adapter for comics inside comic group.

--- app/src/main/java/com/codebosses/comical/endpoints/EndpointKeys.java
+++ app/src/main/java/com/codebosses/comical/endpoints/EndpointKeys.java
@@ -5,6 +5,7 @@ public interface EndpointKeys {
     //    Adapter click keys....
     String HOME_COMIC_CLICK = "home_comic_click";
     String SEARCH_COMIC_CLICK = "search_comic_click";
+    String COMIC_DETAIL_COMIC_CLICK = "comic_detail_comic_click";
 
     //    Intent keys....
     String COMIC = "comic_model";
--- app/src/main/java/com/codebosses/comical/fragments/FragmentComics.java
+++ app/src/main/java/com/codebosses/comical/fragments/FragmentComics.java
@@ -2,6 +2,7 @@ package com.codebosses.comical.fragments;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,9 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.codebosses.comical.R;
+import com.codebosses.comical.databinding.FragmentComicsBinding;
+import com.codebosses.comical.endpoints.EndpointKeys;
+import com.codebosses.comical.pojo.ComicGroup;
 
 public class FragmentComics extends Fragment {
 
+    private FragmentComicsBinding comicsBinding;
+
+    public static FragmentComics getInstance(ComicGroup comicGroup) {
+        FragmentComics fragmentComics = new FragmentComics();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(EndpointKeys.COMIC, comicGroup);
+        fragmentComics.setArguments(bundle);
+        return fragmentComics;
+    }
 
     public FragmentComics() {
 
@@ -21,7 +34,9 @@ public class FragmentComics extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_comics, container, false);
+        comicsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_comics, container, false);
+
+        return comicsBinding.getRoot();
     }
 
 }
